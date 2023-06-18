b70d936a2b4e25ff83199f251208adb3068fceff
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2023-06-18 13:14:03 -0300

    feat: use chakra CSSReset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { CSSReset, ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { theme } from "@/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <CSSReset />
       <Component {...pageProps} />
     </ChakraProvider>
   );
